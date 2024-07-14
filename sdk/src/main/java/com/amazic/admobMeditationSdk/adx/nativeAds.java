c7afc8a9d9bd4a8f6d80c87c3a0793f5abc1bfb6
Chiến <49565292+quangchienictu@users.noreply.github.com>
2024-07-15 02:58:13 +0700

    Update nativeAds.java

--- sdk/src/main/java/com/amazic/admobMeditationSdk/adx/nativeAds.java
+++ sdk/src/main/java/com/amazic/admobMeditationSdk/adx/nativeAds.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.amazic.admobMeditationSdk.util.FirebaseAnalyticsUtil;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
@@ -27,7 +26,6 @@ public class nativeAds implements CustomEventNative {
                                 @Nullable Bundle bundle) {
 
         Log.e(Tag, "ID :"+serverParameter );
-        FirebaseAnalyticsUtil.logEventMediationAdx(context,FirebaseAnalyticsUtil.NATIVE);
         AdLoader adLoader = new AdLoader.Builder(context, serverParameter)
                 .withAdListener(new AdListener() {
                     @Override
